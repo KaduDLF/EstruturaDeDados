5409e68fb30fa744cccdcd442985917da1edf96d
Carlos Eduardo <dev.carlosoliveira@gmail.com>
2025-01-08 11:39:50 -0300

    realloc

--- alocarMemoria.cpp
+++ alocarMemoria.cpp
@@ -5,14 +5,16 @@
 using namespace std;
 
 int main(){
-    int *i;
+    int *i, *p;
     char *c;
 
     i = (int*) malloc(5 * sizeof(int)); // vai alocar 20 bytes, ou seja 5 posições pois 5* 4 = 20
     c = (char*) malloc(200); //aloca 200 espaços pois cada char ocupa apenas 1 byte
-
+    p = (int*) calloc (5, 4/* sizeof(int) */);
+    p = (int*) realloc (p, 40); // aumentando tamanho de p para 10;
+    int *o = (int*) realloc (NULL, 20); // alocando memoria com a função realloc
     cout << *i << endl;
-
+    // o = (int*) realloc (o, 0); libera memoria, semelhante a função free()
     i[1] = 20;
 
     cout << i[1];
@@ -22,12 +24,12 @@ int main(){
         exit(1);
     }
 
-    for( int j = 0; j < 5; j++){
-        cout << "digite o valor de p[" << j+1 << "]" << endl;
-        cin >> i[j];
+    // for( int j = 0; j < 5; j++){
+    //     cout << "digite o valor de p[" << j+1 << "]" << endl;
+    //     cin >> i[j];
 
 
-    }
+    // }
 
     free(i); // liberando a memoria alocada!
 }
